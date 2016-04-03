48a70b356e87f6e1963f2e7f8a15777f9903193e
Will <will@knewto.com>
2016-04-02 21:31:55 -0700

    Created simple Activity with button

--- app/src/main/java/com/knewto/milknote/MainActivity.java
+++ app/src/main/java/com/knewto/milknote/MainActivity.java
@@ -2,6 +2,9 @@ package com.knewto.milknote;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Code to run when Transcribe button is clicked
+        final Button button = (Button) findViewById(R.id.button_id);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                doTranscribe();
+            }
+        });
     }
+
+    private void doTranscribe(){
+        Toast.makeText(getApplicationContext(), "Making Toast", Toast.LENGTH_SHORT).show();
+    }
+
 }
